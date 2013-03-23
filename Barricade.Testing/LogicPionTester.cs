ecb4e39c5bd5d72331c42383b60e6ca512e910fd
T. Slot <tim.slot@gmail.com>
2013-03-23 15:33:10 +0100

    spelboard moet weg

---
+++ Barricade.Testing/LogicPionTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Barricade.Testing
+{
+    [Test]
+    public class LogicPionTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
